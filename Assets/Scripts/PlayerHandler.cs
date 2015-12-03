17959098b13ce49856afb01c516b2965ee3f4cbd
Petri Wilhelmsen <petwil@microsoft.com>
2015-12-03 14:45:23 -0800

    Session 1 commit
    The code after #GameDevLive session 1!

---
+++ Assets/Scripts/PlayerHandler.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerHandler : MonoBehaviour
+{
+    public float speed = 5.0f;
+    public float thrust = 20.0f;
+
+    public ParticleSystem thrustParticles;
+
+    public Text verticalSpeedText;
+
+    bool gameOver = false;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+
+    void OnCollisionEnter(Collision other)
+    {
+        Debug.Log(other.gameObject.tag);
+
+        if(other.gameObject.tag == "Landscape")
+        {
+            if (other.relativeVelocity.magnitude > 1.0)
+            {
+                gameOver = true;
+            }
+        }
+        else if (other.gameObject.tag == "Platform")
+        {
+            if (other.relativeVelocity.magnitude > 2.0)
+            {
+                gameOver = true;
+            }
+        }
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Rigidbody rb = GetComponent<Rigidbody>();
+        if(!gameOver)
+        { 
+	        if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+            {
+                transform.Rotate(Vector3.forward * speed * Time.deltaTime);
+            }
+
+            if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+            {
+                transform.Rotate(Vector3.back * speed * Time.deltaTime);
+            }
+
+            if(Input.GetKey(KeyCode.Space))
+            {
+                rb.AddForce(transform.up * thrust, ForceMode.Acceleration);
+                thrustParticles.Emit(5);
+            }
+        
+            verticalSpeedText.text = "V/S: " + rb.velocity.y.ToString("0.0 m/s");
+        }
+        else
+        {
+            verticalSpeedText.text = "GAME OVER!";
+        }
+
+    }
+}
---
+++ Assets/Scripts/SmoothFollow.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SmoothFollow : MonoBehaviour
+{
+    // The target we are following
+    public Transform target;
+    // The distance in the x-z plane to the target
+    public float distance = 10.0f;
+    // the height we want the camera to be above the target
+    public float height = 5.0f;
+    // How much we 
+    public float heightDamping = 2.0f;
+    public float rotationDamping = 3.0f;
+
+    // Place the script in the Camera-Control group in the component menu
+    [AddComponentMenu("Camera-Control/Smooth Follow")]
+    float timer = 0.0f;
+    float setDistance = 1.0f;
+
+    void Awake()
+    {
+    }
+
+    void FixedUpdate()
+    {
+        timer += Time.deltaTime;
+        setDistance = Mathf.SmoothStep(setDistance, distance, Time.deltaTime * 5.0f);
+
+        // Early out if we don't have a target
+        if (!target) return;
+
+        // Calculate the current rotation angles
+        float wantedRotationAngle = target.eulerAngles.y;
+        float wantedHeight = target.position.y + height;
+
+        float currentRotationAngle = transform.eulerAngles.y;
+        float currentHeight = transform.position.y;
+
+        // Damp the rotation around the y-axis
+        currentRotationAngle = Mathf.LerpAngle(currentRotationAngle, wantedRotationAngle, rotationDamping * Time.deltaTime);
+
+        // Damp the height
+        currentHeight = Mathf.Lerp(currentHeight, wantedHeight, heightDamping * Time.deltaTime);
+
+        // Convert the angle into a rotation
+        var currentRotation = Quaternion.Euler(0, currentRotationAngle, 0);
+
+        // Set the position of the camera on the x-z plane to:
+        // setDistance meters behind the target
+        transform.position = target.position;
+        transform.position -= currentRotation * Vector3.forward * setDistance;
+
+        // Set the height of the camera
+        transform.position = new Vector3(transform.position.x, currentHeight, transform.position.z);
+
+        // Always look at the target
+        transform.LookAt(target);
+    }
+}
