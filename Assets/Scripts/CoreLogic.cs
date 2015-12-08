6b2ba449f241f1b22d3035c8b0e597724d1ff626
Petri Wilhelmsen <petwil@microsoft.com>
2015-12-08 11:38:34 -0800

    Session 2 commit

---
+++ Assets/Scripts/CoreLogic.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoreLogic : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void RestartGame()
+    {
+        Application.LoadLevel(Application.loadedLevel);
+    }
+}
--- Assets/Scripts/PlayerHandler.cs
+++ Assets/Scripts/PlayerHandler.cs
@@ -6,17 +6,33 @@ public class PlayerHandler : MonoBehaviour
 {
     public float speed = 5.0f;
     public float thrust = 20.0f;
+    float fuel = 20.0f;
 
     public ParticleSystem thrustParticles;
 
     public Text verticalSpeedText;
+    public Text fuelText;
+
+    public Button restartGameButton;
 
     bool gameOver = false;
+
+    float fuelTankPickupTimer = 1.0f;
 
     // Use this for initialization
     void Start () {
 	
 	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "FuelTank" && fuelTankPickupTimer < 0.0f)
+        {
+            Destroy(other.gameObject);
+            fuel += 30.0f;
+            fuelTankPickupTimer = 1.0f;
+        }
+    }
 
     void OnCollisionEnter(Collision other)
     {
@@ -41,23 +57,47 @@ public class PlayerHandler : MonoBehaviour
 	
 	// Update is called once per frame
 	void Update () {
+        fuelTankPickupTimer -= Time.deltaTime;
+
         Rigidbody rb = GetComponent<Rigidbody>();
         if(!gameOver)
-        { 
-	        if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            if (fuel > 0.0f)
             {
-                transform.Rotate(Vector3.forward * speed * Time.deltaTime);
+                if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+                {
+                    transform.Rotate(Vector3.forward * speed * Time.deltaTime);
+
+                    fuel -= Time.deltaTime * 1.0f;
+                }
+
+                if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+                {
+                    transform.Rotate(Vector3.back * speed * Time.deltaTime);
+
+                    fuel -= Time.deltaTime * 1.0f;
+                }
+
+                if (Input.GetKey(KeyCode.Space))
+                {
+                    rb.AddForce(transform.up * thrust, ForceMode.Acceleration);
+                    thrustParticles.Emit(5);
+
+                    fuel -= Time.deltaTime * 3.0f;
+                }
             }
 
-            if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+            if(fuel < 20.0f && fuel > 10.0f)
             {
-                transform.Rotate(Vector3.back * speed * Time.deltaTime);
+                fuelText.color = Color.yellow;
             }
-
-            if(Input.GetKey(KeyCode.Space))
+            else if (fuel <= 10.0f)
             {
-                rb.AddForce(transform.up * thrust, ForceMode.Acceleration);
-                thrustParticles.Emit(5);
+                fuelText.color = Color.red;
+            }
+            else
+            {
+                fuelText.color = Color.green;
             }
         
             verticalSpeedText.text = "V/S: " + rb.velocity.y.ToString("0.0 m/s");
@@ -65,7 +105,10 @@ public class PlayerHandler : MonoBehaviour
         else
         {
             verticalSpeedText.text = "GAME OVER!";
+            restartGameButton.gameObject.SetActive(true);
         }
+
+        fuelText.text = "FUEL: " + fuel.ToString("0.0 kg");
 
     }
 }
